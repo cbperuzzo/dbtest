6a83d3f5dfe79f7e132377cc1a68284fcf307ec6
cbperuzzo <caetanob_peruzzo@yahoo.com.br>
2022-10-21 16:49:40 -0300

    quase em dia(bastante coisa)

--- resources/views/livro/index.blade.php
+++ resources/views/livro/index.blade.php
@@ -3,8 +3,25 @@
 @section('content')
     
     <h1>Listagem de livros</h1>
-
-    <a href='livros/create'>novo livro</a>
+    {{Form::open(['url'=>'contatos/buscar','method'=>'GET'])}}
+        <div class="row">
+            <div class="col-sm-3">
+                <a class="btn btn-success" href="{{url('livros/create')}}">Criar</a>
+            </div>
+            <div class="col-sm-9">
+                <div class="input-group ml-5">
+                    @if($busca !== null)
+                        &nbsp;<a class="btn btn-info" href="{{url('livros/')}}">Todos</a>&nbsp;
+                    @endif
+                    {{Form::text('busca',$busca,['class'=>'form-control','required','placeholder'=>'buscar'])}}
+                    &nbsp;
+                    <span class="input-group-btn">
+                        {{Form::submit('Buscar',['class'=>'btn btn-secondary'])}}
+                    </span>
+                </div>
+            </div>
+        </div>
+    {{Form::close()}}
 
     <hr>
 
